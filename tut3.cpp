84c2a9adbf356139975159e2c686c8870d01ac81
Vaibhav <vaibhavtechno5966@gmail.com>
2026-07-28 15:26:13 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//This is a single line comment
+
+/* This is a
+multi line comment
+*/
+
+int main() {
+    int x=365;
+    cout<< "Earth revolves around sun for "<<x << " days";
+    return 0;
+}
